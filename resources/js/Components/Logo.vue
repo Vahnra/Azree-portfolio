29423a5ef8650588f23c48659d059dcac16becde
Vahnra <102360714+Vahnra@users.noreply.github.com>
2022-12-04 21:10:58 +0100

    Initial commit

---
+++ resources/js/Components/Logo.vue
@@ -0,0 +1,9 @@
+<template>
+
+    <div>
+        <div>AZREE</div>
+        <div>ILLUSTRATION</div>
+        <div>PORTFOLIO</div>
+    </div>
+
+</template>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { Head, Link } from '@inertiajs/inertia-vue3';
+import logo from '@/Components/Logo.vue';
+import images from '@/Components/Images.vue'
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+});
+
+</script>
+
+<style>
+body {
+   background-color: #F3F4F6; 
+}
+
+h1 {
+    font-size: 200%;
+    margin-left: 10vw;
+    margin-top: 20vh;
+}
+
+</style>
+
+<template>
+    <Head title="Bienvenue" />
+
+    <!-- <div class="">
+        <div v-if="canLogin" class="">
+            <Link
+                v-if="$page.props.auth.user"
+                :href="route('dashboard')"
+                class=""
+                >Dashboard</Link
+            >
+
+            <template v-else>
+                <Link :href="route('login')" class="">Log in</Link>
+
+            </template>
+        </div>
+
+    </div> -->
+
+    <div class="">
+        <h1>
+            <logo />
+        </h1>
+
+        <images />
+    </div>
+</template>
